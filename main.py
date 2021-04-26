74e162da8fa0aa3dfc7f7036980e5ed5b143c5c8
Honey <69113864+HoneyyDev@users.noreply.github.com>
2021-04-26 15:55:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ async def on_message(message):
     if message.content.startswith(f'{prefix}invite'):
         await message.channel.send("https://discord.gg/bdRHCjsvYV")
 
-    if message.content.startswith(".puceau"):
+    if message.content.startswith(f'{prefix}puceau'):
         await message.channel.send("Puceau moi ? Sérieusement ^^ haha on me l avait pas sortie celle la depuis loooongtemps demande a mes potes si je suis puceau tu vas voir les réponses que tu vas te prendre XD rien que la semaine passee j ai niquer dont chuuuuut ferme la puceau de merde car toi tu m as tout tout l air d un bon puceau de merde car souvent vous etes frustrer de ne pas baiseR ses agreable de se faire un missionnaire ou un amazone avec une meuf hein ? tu peux pas répondre car tu ne sais pas ce que c ou alors tu le sais mais tu as du taper dans ta barre de recherche « missionnaire sexe » ou « amazone sexe » pour comprendre ce que c etait mdddrrr !! cest grave quoiquil en soit....pour revenir a moi, je pense que je suis le mec le moins puceau de ma bande de 11 meilleurs amis pas psk j ai eu le plus de rapport intime mais psk j ai eu les plus jolie femme que mes amis ses pas moi qui le dit, ses eux qui commente sous mes photos insta « trop belle la fille que tu as coucher avec hier en boite notamment! » donc après si tu veux :)")
 
 
